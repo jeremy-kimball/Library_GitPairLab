f45e803856018823a8479c88705c27a975e5eeed
skylarbsandler <skylarbsandler@gmail.com>
2023-05-16 14:37:37 -0600

    Add Patron Class

---
+++ Library_GitPairLab/Patron.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_GitPairLab
+{
+    public class Patron
+    {
+        public string Name;
+        public string PhoneNumber;
+
+        public Patron(string name, string phonenumber)
+        {
+            Name = name;
+            PhoneNumber = phonenumber;
+        }
+    }
+}
